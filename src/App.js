00b8e5f4d8231d25ef9d23c9ee7b5b2332ed533f
Aviad <vocaviad@gmail.com>
2024-02-13 20:54:59 +0200

    change to pasword env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HAHA I KNOW YOUR PASSWORD IT IS {process.env.REACT_APP_PASSWORD}
         </p>
         <a
           className="App-link"
